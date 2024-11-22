79f3e3d5f4a2c59d070518b6cadc0513610438f2
Lydia Abeck Noh <98993226+lydianoh-tech@users.noreply.github.com>
2024-11-22 02:43:47 -0500

    Added index.js to project

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+const initialCards = [
+  {
+    title: "Adam BignellB4",
+    link: "https://unsplash.com/photos/a-close-up-of-a-palm-tree-with-lots-of-leaves-b49yoRQ0JzE",
+  },
+  {
+    title: "Adam Bignell KRB",
+    link: "https://unsplash.com/photos/a-mirror-that-is-in-the-middle-of-some-plants-KRB9mG6qZhw",
+  },
+  {
+    title: "Andrei Popescue",
+    link: "https://unsplash.com/photos/a-church-in-the-middle-of-a-field-with-mountains-in-the-background-FmOPaefLRA0",
+  },
+  {
+    title: "Raspopova Marina",
+    link: "https://unsplash.com/photos/a-close-up-of-a-bunch-of-purple-flowers-aYE-yHHQVBs",
+  },
+  {
+    title: "Taramae Mille",
+    link: "https://unsplash.com/photos/a-trail-in-the-woods-with-lots-of-trees-LH9MLCVHV08",
+  },
+  {
+    title: "Tom Swinnen",
+    link: "https://unsplash.com/photos/a-man-standing-in-front-of-a-waterfall-surrounded-by-trees-7rv04mazX7g",
+  },
+];
+console.log(initialCards);
