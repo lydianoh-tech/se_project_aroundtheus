9e6abcd88575af2eb5338eca0d555a5c31a735b4
Lydia Abeck Noh <98993226+lydianoh-tech@users.noreply.github.com>
2024-11-22 03:20:00 -0500

    correct links

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
-    name: "Adam BignellB4",
-    link: "https://unsplash.com/photos/a-close-up-of-a-palm-tree-with-lots-of-leaves-b49yoRQ0JzE",
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
   {
-    name: "Adam Bignell KRB",
-    link: "https://unsplash.com/photos/a-mirror-that-is-in-the-middle-of-some-plants-KRB9mG6qZhw",
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
   {
-    name: "Andrei Popescue",
-    link: "https://unsplash.com/photos/a-church-in-the-middle-of-a-field-with-mountains-in-the-background-FmOPaefLRA0",
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg,
   },
   {
-    name: "Raspopova Marina",
-    link: "https://unsplash.com/photos/a-close-up-of-a-bunch-of-purple-flowers-aYE-yHHQVBs",
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
   {
-    name: "Taramae Mille",
-    link: "https://unsplash.com/photos/a-trail-in-the-woods-with-lots-of-trees-LH9MLCVHV08",
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
   {
-    name: "Tom Swinnen",
-    link: "https://unsplash.com/photos/a-man-standing-in-front-of-a-waterfall-surrounded-by-trees-7rv04mazX7g",
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
 console.log(initialCards);
