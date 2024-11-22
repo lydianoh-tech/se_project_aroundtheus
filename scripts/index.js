b7f077ef711d9722eb74ee8cd5e1d8e19405125e
Lydia Abeck Noh <98993226+lydianoh-tech@users.noreply.github.com>
2024-11-22 02:50:09 -0500

    Correct variable naming

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 const initialCards = [
   {
-    title: "Adam BignellB4",
+    name: "Adam BignellB4",
     link: "https://unsplash.com/photos/a-close-up-of-a-palm-tree-with-lots-of-leaves-b49yoRQ0JzE",
   },
   {
-    title: "Adam Bignell KRB",
+    name: "Adam Bignell KRB",
     link: "https://unsplash.com/photos/a-mirror-that-is-in-the-middle-of-some-plants-KRB9mG6qZhw",
   },
   {
-    title: "Andrei Popescue",
+    name: "Andrei Popescue",
     link: "https://unsplash.com/photos/a-church-in-the-middle-of-a-field-with-mountains-in-the-background-FmOPaefLRA0",
   },
   {
-    title: "Raspopova Marina",
+    nane: "Raspopova Marina",
     link: "https://unsplash.com/photos/a-close-up-of-a-bunch-of-purple-flowers-aYE-yHHQVBs",
   },
   {
-    title: "Taramae Mille",
+    name: "Taramae Mille",
     link: "https://unsplash.com/photos/a-trail-in-the-woods-with-lots-of-trees-LH9MLCVHV08",
   },
   {
-    title: "Tom Swinnen",
+    name: "Tom Swinnen",
     link: "https://unsplash.com/photos/a-man-standing-in-front-of-a-waterfall-surrounded-by-trees-7rv04mazX7g",
   },
 ];
